f000da1fe81c54176078746ec2fbf5c7d222424f
Yelim Kim <Yelim.KIM@student.tafesa.edu.au>
2025-03-17 13:16:36 +1030

    Commit the branch feat_GST1.2

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25);
                 totalWarpChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25 + 5);
+                totalGSTChargeTextBlock.Text = Convert.ToString((cProduct.TotalPayment + 25 + 5)*1.1m);
             }
             catch (FormatException)
             {
